5ff74e3c3ab525d1e70295acc671ec96e3c14d74
ALSHA BEHERA <alshabehera7@gmail.com>
2023-08-19 16:44:18 +0530

    todoList

--- tododlist/app.js
+++ tododlist/app.js
@@ -2,34 +2,105 @@ import express from "express";
 import bodyParser from "body-parser";
 //import {dirname} from "path";
 //import { fileURLToPath } from "url";
-import { getDate, getDay } from './date.js';
+
+import mongoose from 'mongoose';
+
+
 const app=express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 const port=3000;
+
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB",{useNewUrlParser:true, useUnifiedTopology: true });
+const itemSchema=new mongoose.Schema({
+    name:String,
+})
+const Item=mongoose.model('Item',itemSchema);
+
+const item1= new Item({
+    name:"Welcome to todo list",
+});
+const item2=new Item({
+    name:"hit + button to add an extra todo",
+});
+const item3=new Item({
+    name:"<---hit to delete this item",
+})
+
+
+
+async function itemInsert(){
+ try{await Item.insertMany([item1,item2,item3]);
+ console.log("Successfully insert");
+}catch(err){
+    console.log(err);
+}finally{
+    mongoose.disconnect();
+}
+}
+//itemInsert();
 const items=["buy a new bf","buy a new clg","die"];
 const workItems = [];
 
-const day = getDate();
 
-app.get("/",(req,res)=>{
-    res.render("list.ejs",{listTitle:day,list:items});
+
+app.get("/",async(req,res)=>{
+   
+try{
+       const items=await Item.find({});
+        console.log(items);
+        if(items.length===0){
+            itemInsert();res.redirect("/");
+
+        }
+        else{
+           res.render("list.ejs",{listTitle:"today",list:items});
+        }
+       
+    }catch(err){
+        console.log(err);
+    }
+    
+
 });
 app.get("/work", (req,res)=>{
     res.render("list.ejs", {listTitle: "work", list: workItems});
   });
+app.post("/", async (req, res) => {
+    const newItem = req.body.NewItem;
+    const listTitle = req.body.list;
 
-app.post("/",(req,res)=>{
-    const newItem=req.body.NewItem;
-    if(req.body.list==="work"){
-        workItems.push(newItem);
-        res.redirect("/work");
+    try {
+        if (listTitle === "work") {
+            // Handle work items differently
+            // ...
+        } else {
+            const item = new Item({ name: newItem });
+            await item.save();
+        }
+        res.redirect("/");
+    } catch (err) {
+        console.log(err);
     }
-    else{items.push(newItem);
-    res.redirect("/");}
+});
+//app.post("/",(req,res)=>{
+    //const newItem=req.body.NewItem;
+    //if(req.body.list==="work"){
+      //  workItems.push(newItem);
+      //  res.redirect("/work");
+    //}
+    //else{items.push(newItem);
+   // res.redirect("/");}
+   
+    //console.log(newItem);
+    //const iteme=new Item({
+        //name:newItem,
+   // });
+   // iteme.save();
+    //res.redirect("/");
 
-})
+//})
 
   
 
